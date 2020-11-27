47c8b53ce71ad7065384be5e9d065991e283e79a
cydf-jyh <yeonghoon12@korea.ac.kr>
2020-11-27 00:08:57 -0800

    Add echo-client.cpp

--- echo-client.cpp
+++ echo-client.cpp
@@ -1,89 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <iostream>
 #include <thread>
-
+#include <stdlib.h>
+#include <signal.h>
 using namespace std;
-
-void usage() {
-	cout << "syntax: echo-client <ip> <port>\n";
-	cout << "sample: echo-client 127.0.0.1 1234\n";
-}
-
-struct Param {
-	struct in_addr ip{0};
-	uint16_t port{0};
-
-	bool parse(int argc, char* argv[]) {
-		for (int i = 1; i < argc; i++) {
-			int res = inet_pton(AF_INET, argv[i++], &ip);
-			switch (res) {
-				case 1: break;
-				case 0: cerr << "not a valid network address\n"; return false;
-				case -1: perror("inet_pton"); return false;
-			}
-			port = stoi(argv[i++]);
-		}
-		return (ip.s_addr != 0) && (port != 0);
-	}
-} param;
-
-void recvThread(int sd) {
-	cout << "connected\n";
-	static const int BUFSIZE = 65536;
-	char buf[BUFSIZE];
+#define BUFFERSIZE 1024
+int close_fd;
+void recvThread(int client_fd) {
+	printf("RecvThread is working!\n");
+	char buf[BUFFERSIZE];
 	while (true) {
-		ssize_t res = recv(sd, buf, BUFSIZE - 1, 0);
+		ssize_t res = recv(client_fd, buf, BUFFERSIZE - 1, 0);
 		if (res == 0 || res == -1) {
-			cerr << "recv return " << res << endl;
-			perror("recv");
+			perror("recv failed!");
 			break;
 		}
 		buf[res] = '\0';
-		cout <<"server: "<<buf << endl;
+		printf("message sent from server : %s\n",buf);
 	}
-	cout << "disconnected\n";
-    close(sd);
+	printf("disconnected!\n");
+	close(client_fd);
 	exit(0);
 }
-
-int main(int argc, char* argv[]) {
-	if (!param.parse(argc, argv)) {
-		usage();
-		return -1;
+int main(int argc, char **argv) {
+	int client_fd;
+	struct sockaddr_in server_address;
+	if (argc != 3) {
+		printf("echo-client:\n");
+		printf("syntax : echo-client <ip> <port>\n");
+		printf("sample : echo-client 127.0.0.1 1234\n");
+		return 0;
 	}
-
-	int sd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sd == -1) {
-		perror("socket");
-		return -1;
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(client_fd == -1){
+		printf("socket failed!\n");
+		return 0;
 	}
-
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(param.port);
-	addr.sin_addr = param.ip;
-	memset(&addr.sin_zero, 0, sizeof(addr.sin_zero));
-
-	int res = connect(sd, (struct sockaddr *)&addr, sizeof(addr));
-	if (res == -1) {
-		perror("connect");
-		return -1;
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	if(!inet_aton(argv[1], &server_address.sin_addr)){
+		printf("Invalid Server Ip address!\n");
+		return 0;
 	}
-
-	thread t(recvThread, sd);
-	t.detach();
-	while (true) {
-		string s;
-		getline(cin, s);
-		ssize_t res = send(sd, s.c_str(), s.size(), 0);
+	server_address.sin_port = htons(atoi(argv[2]));
+	int flag=connect(client_fd, (struct sockaddr*)&server_address, sizeof(server_address));
+	if(flag==-1){
+		printf("Connection disabled!\n");
+		return 0;
+	}
+	printf("Client program connection established\n\n");
+	close_fd=client_fd;
+	thread thread_client_recv(recvThread, client_fd);
+	thread_client_recv.detach();
+	while(true){
+		char message[BUFFERSIZE];
+		int message_length;
+		scanf("%s",message);
+		message_length=strlen(message);
+		ssize_t res = send(client_fd, message, message_length, 0);
 		if (res == 0 || res == -1) {
-			cerr << "send return " << res << endl;
-			perror("send");
+			perror("send failed!");
 			break;
 		}
 	}
-	close(sd);
+	close(client_fd);
+	return 0;
 }
